3cc088f1db6849189e975dbbe9dc043c8b92e964
tuanlinh <trantuanlinht@gmail.com>
2014-08-18 00:30:57 +0700

    ok

--- fuel/vendor/composer/installers/src/Composer/Installers/RedaxoInstaller.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Composer\Installers;
-
-class RedaxoInstaller extends BaseInstaller
-{
-    protected $locations = array(
-        'addon'    => 'redaxo/include/addons/{$name}/',
-    );
-}
